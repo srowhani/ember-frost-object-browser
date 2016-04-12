6feeeafc88d4ca8d54695023c4a77f0e61ed00f2
Adam Meadows <adam.meadows@gmail.com>
2016-04-12 12:41:29 -0700

    Consuming latest bunsen
    This is a #MAJOR# change, merely because we're consuming a new major version
    of bunsen.

--- blueprints/ember-frost-object-browser/index.js
+++ blueprints/ember-frost-object-browser/index.js
@@ -11,8 +11,8 @@ module.exports = {
   afterInstall: function () {
     return this.addAddonsToProject({
       packages: [
-        {name: 'ember-frost-core', target: '>=0.0.14 <2.0.0'},
-        {name: 'ember-frost-bunsen', target: '^3.0.0'},
+        {name: 'ember-frost-core', target: '>=0.1.0 <2.0.0'},
+        {name: 'ember-frost-bunsen', target: '^4.0.0'},
         {name: 'ember-frost-info-bar', target: '^2.0.0'},
         {name: 'ember-frost-list', target: '>=0.5.0 <2.0.0'}
       ]
