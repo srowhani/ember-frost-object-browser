1ebf7768bb5d42bfc7332cfa62bd117e8c795ab7
Matthew Dahl <mdahl@ciena.com>
2016-05-24 21:18:08 -0700

    Upgrade ember-prop-types and ember-frost-bunsen

--- blueprints/ember-frost-object-browser/index.js
+++ blueprints/ember-frost-object-browser/index.js
@@ -13,9 +13,10 @@ module.exports = {
       packages: [
         {name: 'ember-block-slots', target: '>=0.12.1 <2.0.0'},
         {name: 'ember-frost-core', target: '>=0.2.1 <2.0.0'},
-        {name: 'ember-frost-bunsen', target: '^5.0.0'},
+        {name: 'ember-frost-bunsen', target: '^6.0.0'},
         {name: 'ember-frost-info-bar', target: '^2.0.0'},
         {name: 'ember-frost-list', target: '>=0.5.0 <2.0.0'},
+        {name: 'ember-prop-types', target: '^2.0.0'},
         {name: 'liquid-fire', target: '>=0.23.0 <2.0.0'}
       ]
     })
