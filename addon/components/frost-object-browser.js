5144be31c55da8c847fd2d17009b0a175db9e318
Michael Carroll <juwara0@users.noreply.github.com>
2016-06-09 10:13:43 -0500

    added docblocks for new frost-browser implementation pieces

--- addon/components/frost-object-browser.js
+++ addon/components/frost-object-browser.js
@@ -7,21 +7,49 @@ const {
   Component
 } = Ember
 
+/**
+ * @module
+ * @augments ember/Component
+ * @augments module:ember-prop-types
+ */
 export default Component.extend({
+
+  /** @type {String[]} */
   classNames: ['frost-object-browser'],
+
+  /** @type {Object} */
   layout,
+
+  /** @type {Object} */
   propTypes: {
     selections: PropTypes.array
   },
 
+  /**
+   * Sets the properties with their default values
+   *
+   * @function
+   * @return {Object} The properties with their default values
+   */
   getDefaultProps () {
     return {
       selections: A([])
     }
   },
 
+  /**
+   * @type {Object}
+   */
   actions: {
     // TODO Decide on an interface for selection data
+
+    /**
+     * Handles setting/clearing the user's selections in the view
+     *
+     * @function actions:onSelect
+     * @param {Object} viewSelections record that was just selected/deselected
+     * @returns {undefined}
+     */
     onSelect (viewSelections) {
       const selections = this.get('selections')
       selections.clear()
--- addon/components/selection-action-button.js
+++ addon/components/selection-action-button.js
@@ -7,13 +7,31 @@ const {
   ViewUtils
 } = Ember
 
+/**
+ * @module
+ * @augments module:ember-frost-core/components/frost-button
+ */
 export default FrostButton.extend({
+
+  /**
+   * Sets the properties with their default values
+   *
+   * @function
+   * @return {Object} The properties with their default values
+   */
   getDefaultProps () {
     const defaults = this._super()
     delete defaults.disabled
     return defaults
   },
 
+  /**
+   * Determines whether the button is disabled based on the user having items selected in the view slot.
+   * If multiSelect is NOT set then only one item can be selected or the button will be disabled.
+   *
+   * @function
+   * @return {Boolean} Should this button be disabled
+   */
   disabled: computed('selections.[]', function () {
     const length = this.get('selections.length')
     if (this.get('multiSelect')) {
@@ -23,6 +41,14 @@ export default FrostButton.extend({
     }
   }),
 
+  /**
+   * Sets up behavior for onClick event
+   *
+   * @function
+   * @param  {Object} event The mouse click event
+   * @return {Boolean} If user does Shift|Ctrl|Alt|Meta|Secondary + click, then exit the function
+   * without doing the logic to execute the closure action
+   */
   onclick: Ember.on('click', function (event) {
     if (!ViewUtils.isSimpleClick(event)) {
       return true
