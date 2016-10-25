4084ee5454766c36897843ce9a4eb0742e9985c7
Quincy <qle@ciena.com>
2016-10-25 10:32:55 -0400

    added index.js in addon for easier module export

---
+++ addon/index.js
@@ -0,0 +1,16 @@
+import ObjectBrowserMixin from './mixins/object-browser-mixin'
+import ObjectBrowserRouteMixin from './mixins/object-browser-route-mixin'
+import ObjectBrowserSerializer from './modules/object-browser-serializer'
+import JsonApiObjectBrowserSerializer from './modules/json-api-object-browser-serializer'
+import OffsetPagination from './modules/pagination/offset'
+
+export {
+  ObjectBrowserMixin,
+  ObjectBrowserRouteMixin,
+  ObjectBrowserSerializer,
+  JsonApiObjectBrowserSerializer,
+  OffsetPagination
+}
+
+
+
---
+++ tests/dummy/app/serializers/resource.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+
+  normalizeArrayResponse (store, primaryModelClass, payload, id, requestType) {
+
+    debugger;
+  }
+
+});
