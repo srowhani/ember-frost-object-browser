014b1476312ffc129beb7c547262f3107aba24f0
Quincy <qle@ciena.com>
2016-11-08 15:07:44 -0500

    remove logic-or helper

--- addon/helpers/logic-or.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export function logicOr(params/*, hash*/) {
-  if (params.length !== 2) Ember.Logger.error(`logic-or helper expects 2 arguments, received ${params.length}`)
-
-  return params[0] || params[1]
-}
-
-export default Ember.Helper.helper(logicOr);
