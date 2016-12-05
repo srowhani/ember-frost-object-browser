1e2fa7482129026bdc42d5f60ba4a8a66f4feff5
Quincy <qle@ciena.com>
2016-12-05 01:48:49 -0500

    imporved action closure to cover attrs curry

--- addon/utils/action-closure.js
+++ addon/utils/action-closure.js
@@ -1,9 +1,42 @@
-export function create (method) {
+//export function create (method) {
+//  let context = this
+//  return function wrapperFunc () {
+//    return method.apply(context, arguments)
+//  }
+//}
+//
+//export default create
+
+
+export function create (action, actionArgs = []) {
+  //TODO  type check actionArgs should be array.
   let context = this
-  return function wrapperFunc () {
-    return method.apply(context, arguments)
+  let actionArgsLength = actionArgs.length
+  let wrapperFunc
+
+  if (actionArgsLength > 0) {
+    wrapperFunc = function (...passedArguments) {
+      let args = new Array(actionArgsLength + passedArguments.length)
+
+      for (let i = 0; i < actionArgsLength; i++) {
+        args[i] = actionArgs[i]
+      }
+
+      for (let i = 0; i < passedArguments.length; i++) {
+        args[i + actionArgsLength] = passedArguments[i]
+      }
+
+      return action.apply(context, args)
+    }
+  } else {
+    wrapperFunc = function () {
+      return action.apply(context, arguments)
+    }
   }
+
+  return wrapperFunc
 }
 
-export default create
 
+
+export default create
